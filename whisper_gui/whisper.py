3f4530f35e9bdeb315b86bb3398a8d2d9f6bbc64
jayant yadav <jayantyadav202@gmail.com>
2024-05-23 08:40:04 +0200

    model paths and torch type correction

--- whisper_gui/whisper.py
+++ whisper_gui/whisper.py
@@ -22,24 +22,29 @@ if __name__ == "__main__":
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    # model_id = "openai/whisper-tiny"
-    model_id = "openai/whisper-large-v2"
+    model_id = "openai/whisper-tiny"
+    # model_id = "openai/whisper-large-v2"
+    # model_id = "../models/models--openai--whisper-large-v2"
+
     # model_id = "openai/whisper-tiny"
     # assistant_model_id = "distil-whisper/distil-large-v2"
     assistant_model_id = "openai/whisper-tiny"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id,
+        model_id, # chnage to pretrained_model_name_or_path = '../models/'
+        cache_dir = "./models/",
         torch_dtype=torch_dtype,
         low_cpu_mem_usage=True,
         use_safetensors=True,
         attn_implementation="sdpa",
+        
     )
     model.to(device)
 
     # Use AutoModelForCausalLM in case the encoder is same like distil-large-v2
     assistant_model = AutoModelForSpeechSeq2Seq.from_pretrained( 
         assistant_model_id,
+        cache_dir = "./models/",
         torch_dtype=torch_dtype,
         low_cpu_mem_usage=True,
         use_safetensors=True,
@@ -61,10 +66,10 @@ if __name__ == "__main__":
     for sample in tqdm(dataset):
         audio = sample["audio"]
         inputs = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt")
-        inputs = inputs.to(device=device, dtype=torch.float32) #half precision (float16) does not work. Use float32.
+        inputs = inputs.to(device=device, dtype=torch_dtype) #half precision (float16) does not work. Use float32.
         
-        # output, gen_time = generate_with_time(model, inputs)
-        output, gen_time = assisted_generate_with_time(model, inputs, language="nl", task="transcribe")
+        output, gen_time = generate_with_time(model, inputs, language="nl", task="transcribe")
+        # output, gen_time = assisted_generate_with_time(model, inputs, language="nl", task="transcribe")
         all_time += gen_time
         predictions.append(processor.batch_decode(output, skip_special_tokens=True, normalize=True)[0])
         # references.append(processor.tokenizer._normalize(sample["text"]))
